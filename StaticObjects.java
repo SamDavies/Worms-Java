d91f85126321281940dbd5e5db62c94fba1a7b9a
Kostadin Kalinov <kostadim@abv.bg>
2013-02-12 02:49:52 +0000

    Static objects class

--- StaticObjects.java
+++ StaticObjects.java
@@ -1,4 +1,15 @@
+import java.awt.Image;
 
-public class StaticObjects {
 
+public class StaticObjects {     //the class for the static objects on the screen
+    
+	Image objectImage; //image for the object
+	int x,y; //x and y coordinate for drawing the image on the screen
+	int imageWidth,imageHeight; //width and height of the image of the object
+	
+	public StaticObjects(){		//constructor
+		
+	}
+	
 }
+
