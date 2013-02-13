a3adc0a1fa0656fa790f65b055bd2af3eced8cbc
Sam Davies <samdavies@live.co.uk>
2013-02-13 16:08:52 +0000

    missile position
    updates the position with respect to time and rounds in to int for
    coordinates.

--- ReactiveObjects.java
+++ ReactiveObjects.java
@@ -12,5 +12,24 @@ public class ReactiveObjects {
 	
 	}
 	
+	public static int[] missileMovement(int x, int y, int time){ //function which calculates the new position after a time
+		
+		int t = time;	//starts a clock to update the missile
+		double u = 10; //Initial speed of missile
+		double a = -10; //acceleration due to gravity
+		
+				
+		x = x + 10; // creates a constant horizontal speed 
+					
+		double s = u*t + 0.5 * a * t * t; //equation for vertical position at a given time
+				
+		y = y + (int)Math.round(s); //old position plus change in position
+		
+		int[] xy = {x, y}; //out put coordinates
+		
+		return xy;				
+		
+	 }
+	
 	
 }
