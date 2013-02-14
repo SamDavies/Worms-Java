5c44369996256cd17b84986afed0505eeb2bb6bf
Kostadin Kalinov <kostadim@abv.bg>
2013-02-14 03:09:46 +0000

    the missile class

---
+++ Missile.java
@@ -0,0 +1,24 @@
+import java.awt.Image;
+
+
+public class Missile extends ReactiveObjects {
+
+
+	public Missile(int xvalue, int yvalue, Image[] i) {
+		super(xvalue, yvalue, i); //uses the constructor of ReactiveObjects class
+		
+	}
+
+	public static void main(String[] args) {     //we are going to move the missile here
+		
+    //sample code
+	//loop{
+    //	 if(missile.checkcollisionStatic()==false)
+	//   if(missile.checkcollisionReactive()==false)	  
+    //      missle.move();
+	//   else missile.destroy(ReactiveObject r);
+	//	 else missile.destroy();
+	//	}	
+	}
+
+}
